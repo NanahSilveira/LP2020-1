2fa2c994ed2a0ca24ebf697b0d8239c5474c9516
Tainara Silveira <tainarasilveirakpop@gmail.com>
2021-03-06 22:17:09 +0000

    SEMANA02_q05.c

---
+++ SEMANA02_q05.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+int numantecessor, numsucessor;
+printf("lista ordenada com seus respectivos antecessores e sucessores ");
+scanf("%i%i",&numantecessor,&numsucessor);
+printf("2 %i 4 8 %i 10\n",numantecessor,numsucessor);
+scanf("%i%i",&numantecessor,&numsucessor);
+printf("1 %i %i 4\n",numsucessor,numantecessor);
+
+return 0;
+}
