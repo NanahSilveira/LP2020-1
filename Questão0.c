1800816c9bea7ce6ac963f494751e365619616ad
Tainara Silveira <tainarasilveirakpop@gmail.com>
2021-03-21 20:16:50 +0000

    Questão0.c

--- Questão0.c
+++ Questão0.c
@@ -1,13 +1,7 @@
 // #include<stdio.h>
 // int main(void){
 //   int A,B;
-//   printf("Dois numeros inteiros: ");
 //   scanf("%i%i",&A,&B);
-//   if(A>B){
-//     printf("\n%i %i: ",B,A);
-//   }
-//   if(A==B){
-//     printf("\n%i %i: ",B,A);
+//   printf("\n%i %i: ",A,B);
 //   return 0;
-// }
 // }
