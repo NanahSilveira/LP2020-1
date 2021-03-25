95146a07c7cbdb5d35a784158820e4289430b002
Tainara Silveira <tainarasilveirakpop@gmail.com>
2021-03-25 23:18:49 +0000

    Questão1.c

---
+++ Questão1.c
@@ -0,0 +1,18 @@
+// #include<stdio.h>
+// int main(void){
+//   int Num,Num2;
+//   printf("Dois numeros inteiros: \n");
+//   scanf("%i%i",&Num,&Num2);
+// if(Num<Num2){
+//   printf("2 3\n");
+// }else{
+//   if(Num>Num2){
+//   printf("2 3\n");
+// }else{
+//   if(Num==Num2){
+//   printf("2 2\n");
+// }
+// return 0;
+//   }
+//  }
+// }
