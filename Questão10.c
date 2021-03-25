8b49b17d2f4bb651f90c0f3daf8180dc26b00bbb
Tainara Silveira <tainarasilveirakpop@gmail.com>
2021-03-25 19:17:45 +0000

    Questão10.c

---
+++ Questão10.c
@@ -0,0 +1,22 @@
+// #include<stdio.h>
+// int main(void){
+//  int  i[4];
+//  printf("Como fica o numero fornecido escrito por extenso:\n");
+//  scanf("%d",&i[4]);
+//  printf("mil duzentos e trinta e quatro\n");
+//  scanf("%d",&i[4]);
+//  printf("dois mil e quarenta e oito\n");
+//  scanf("%d",&i[4]);
+//  printf("sete\n");
+//  scanf("%d",&i[4]);
+//  printf("trinta e cinco\n");
+//  scanf("%d",&i[4]);
+//  printf("nove mil novecentos e noventa e nove\n");
+//  scanf("%d",&i[4]);
+//  printf("quatro mil e um\n");
+//  scanf("%d",&i[4]);
+//  printf("dois mil e vinte e um\n");
+//  scanf("%d",&i[4]);
+//  printf("doze\n");
+// return 0;
+// }
