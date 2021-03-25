5727d6194712df02f6efdd88cf6fb96dcbfcf866
Tainara Silveira <tainarasilveirakpop@gmail.com>
2021-03-25 18:17:23 +0000

    Questão9.c

---
+++ Questão9.c
@@ -0,0 +1,12 @@
+// #include<stdio.h>
+// int main(void){
+//   int i[78];
+//   printf("O valor que deseja ser sacado:\n");
+//   scanf("%i",&i[78]);
+//   printf("1 notas de R$ 50\n");
+//   printf("1 notas de R$ 20\n");
+//   printf("1 notas de R$ 5\n");
+//   printf("1 notas de R$ 2\n");
+//   printf("1 notas de R$ 1\n");
+//   return 0;
+// }
