34309a871881af7894d552fe4df22a4cbc31ff40
Tainara Silveira <tainarasilveirakpop@gmail.com>
2021-03-30 21:50:27 +0000

    Lista de exercício7.c

---
+++ Lista de exercício7.c
@@ -0,0 +1,13 @@
+// #include<stdio.h>
+// int main(void){
+//   int num, centenas, dezenas, unidades;
+//   scanf("%i",&num);
+//   centenas= num/100;
+//   printf("%i centenas\n:",centenas);
+//   num=num-centenas*100;
+//   dezenas=num/10;
+//   printf("%i dezenas\n:",dezenas);
+//   unidades=num-dezenas*10;
+//   printf("%i unidades\n:",unidades);
+// return 0;
+// }
