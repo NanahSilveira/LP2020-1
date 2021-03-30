a01322b54d9ae24422fb7f189628d3e925eaed52
Tainara Silveira <tainarasilveirakpop@gmail.com>
2021-03-30 19:16:03 +0000

    Lista de exercício3.c

---
+++ Lista de exercício3.c
@@ -0,0 +1,16 @@
+// #include<stdio.h>
+// int main(void){
+//   int Idade;
+//   printf("Qual a sua idade?\n");
+//   scanf("%i",&Idade);
+//   if(Idade>=10 && Idade<=14){
+//     printf("Infantil\n");
+//   }
+//   if(Idade>=15 && Idade<=17){
+//     printf("Juvenil\n");
+//   }
+//   if(Idade>=18 && Idade<=25){
+//     printf("Adulto\n");
+//   }
+//   return 0;
+// }
