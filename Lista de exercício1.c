74568ce108cd3757e13f9cf648ad33281bdf2430
Tainara Silveira <tainarasilveirakpop@gmail.com>
2021-03-30 18:53:18 +0000

    Lista de exercício1.c

---
+++ Lista de exercício1.c
@@ -0,0 +1,16 @@
+// #include<stdio.h>
+// int main(void){
+//   float N1,N2,N3,média;
+//   scanf("%f %f %f",&N1,&N2,&N3);
+//   média=(N1*2+N2*3+N3*4)/9;
+//   if(média>=7){
+//     printf("Aprovado\n");
+//   }else{
+//     if(média<3.5){
+//     printf("Reprovado\n");
+//   }else{
+//       printf("Recuperação\n");
+//     }
+//   }
+//   return 0;
+//   }
