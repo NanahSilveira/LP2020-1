19d3b5b7d8e07f96b94c84d15c2d73115a630445
Tainara Silveira <tainarasilveirakpop@gmail.com>
2021-03-30 19:08:07 +0000

    Lista de exercício2.c

---
+++ Lista de exercício2.c
@@ -0,0 +1,12 @@
+// #include<stdio.h>
+// int main(void){
+//   int Idade;
+//   printf("Qual a sua idade?:\n");
+//   scanf("%i",&Idade);
+//   if(Idade>=18 && Idade<=67){
+//     printf("Pode doar\n");
+//   }else{
+//     printf("Não pode\n");
+//   }
+//   return 0;
+// }
